bdfa87857aef9ec282665e65261d85c19ba81862
Nazym-MU <nazym@uni.minerva.edu>
2025-03-03 16:55:52 +0900

    Add exit modal

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -1,14 +1,16 @@
 import { Progress } from "@/components/ui/progress";
 import { X } from "lucide-react";
+import { useExitModal } from "@/store/use-exit-modal";
 
 type Props = {
     percentage: number;
 };
 
 export const Header = ({ percentage }: Props) => {
+    const { open } = useExitModal();
     return (
         <header className="lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full"> 
-            <X onClick={() => {}} className="text-slate-500 hover:opacity-75 transition cursor-pointer" />
+            <X onClick={open} className="text-slate-500 hover:opacity-75 transition cursor-pointer" />
             <Progress value={percentage} />
         </header>
     );
